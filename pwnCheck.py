80d25c70017b70585a6229bb72aac1349997954a
0xpr0N3rd <celik.anil@student.atilim.edu.tr>
2021-03-16 06:46:33 -0400

    optimization & addition of output module
    Output module has been added.

--- pwnCheck.py
+++ pwnCheck.py
@@ -1,63 +1,89 @@
 #!/usr/bin/python3
 
-import requests, sys, time, json
+import requests, sys, time, json, functools, operator, contextlib
 
-headers = {
-	'User-Agent': 'agent_smith',
-	'Accept': 'accept_em_all',
-	'Accept-Language': 'language',
-	'Cookie': 'cookie_monster'
-}
+def convertResponse(response): # convert tuple objects into string for sanitized output
+	return functools.reduce(operator.add, (response)) 
 
-result = []
+def main():
+	headers = {
+		'User-Agent': 'agent_smith',
+		'Accept': 'accept_em_all',
+		'Accept-Language': 'language',
+		'Cookie': 'cookie_monster'
+	}
 
-with open(sys.argv[1], 'r') as inputFile:
-	time.sleep(1)
-	print("\n[!] File is being read...")
-	time.sleep(1)
-	print("[!] Performing requests...\n")
-	print("-----------------------\n")
-	
-	ct = 0
-	
-	for line in inputFile:
-		URL = 'https://haveibeenpwned.com/unifiedsearch/' # request URL
-		URL += line # append mail to the URL
-		r = requests.get(URL.strip(), headers=headers) # request object
-		time.sleep(2) # prevent getting kicked out because of the rate limit
+	result = []
+	lineBreaker = "-----------------------\n"
+
+	with open(sys.argv[1], 'r') as inputFile:
+		outputFile = open(sys.argv[2], 'w')
+		time.sleep(1)
+		print("\n[!] File is being read...")
+		time.sleep(1)
+		print("[!] Performing requests...\n")
+		print(lineBreaker)
 		
-		i = 0 # hold index counter
-		j = 0 # breach iterator
-		breach_ct = 0 # hold breach counter
+		ct = 0
 		
-		if r.status_code == 200: # if status code is 200, it means mail is leaked 
-			print("[+] Found: ", line)
-			result.append(line)
+		for line in inputFile:
+			URL = 'https://haveibeenpwned.com/unifiedsearch/' # request URL
+			URL += line # append mail to the URL
+			r = requests.get(URL.strip(), headers=headers) # request object
+			time.sleep(2) # prevent getting kicked out because of the rate limit
 			
-			json_object = json.loads(r.text)
+			i = 0 # hold index counter
+			j = 0 # breach iterator
+			breach_ct = 0 # hold breach counter
 			
-			for j in json_object['Breaches']:
-				breach_ct += 1
-			
-			for j in range(breach_ct):
-				print("Leak", i+1, ":", json_object['Breaches'][i]['Name'], "- Breach Date:", json_object['Breaches'][i]['BreachDate'])
+			if r.status_code == 200: # if status code is 200, it means mail is leaked 
+				succMsg = "[+] Found: ", line
+				print(convertResponse(succMsg))
+				result.append(line)
+	
+				json_object = json.loads(r.text)
 				
-				i += 1	
-			
-			print("\n-----------------------\n")
+				for j in json_object['Breaches']: # count breach/breaches
+					breach_ct += 1
+				
+				for j in range(breach_ct): # iterate through breach to obtain the data 
+					leakMsg = "Leak ", str(i+1), ": ", json_object['Breaches'][i]['Name'], " - Breach Date: ", json_object['Breaches'][i]['BreachDate'] # integer to string parsing for concatenation error prevention
+					print(convertResponse(leakMsg), "\n")
 					
-		elif r.status_code == 429:
-			print("[!] Seems like you got kicked out because of the rate limit :/")
-	
-		else:
-			print("[-] Not Found: ", line)
-			print("-----------------------\n")
+					i += 1	
+				
+				print(lineBreaker)
+				
+				with contextlib.redirect_stdout(outputFile): # redirect result to the output file
+					print(convertResponse(succMsg))
+					print(convertResponse(leakMsg), "\n")
+					print(lineBreaker)
+					
+						
+			elif r.status_code == 429:
+				limitMsg = "[!] This isn't supposed to be happening but seems like you got kicked out because of the rate limit :/ (Status Code: 429)" 
+				print(limitMsg)
 		
-		ct+=1
+			else:
+				notFound = "[-] Not Found: ", line
+				print(convertResponse(notFound))
+				print(lineBreaker)
+			
+			ct+=1
+		
+		firstResult = "[!] Total number of mails: ", str(ct)
+		print(convertResponse(firstResult))
+		
+		secondResult = "[!] Total number of leaked mails: ", str(len(result))
+		print(convertResponse(secondResult))
+		
+		with contextlib.redirect_stdout(outputFile): # redirect result to the output file
+			print(convertResponse(firstResult))
+			print(convertResponse(secondResult))
+		
+		print("\nPwned mails & breach information are written into:", sys.argv[2])
+		
+		time.sleep(2)
 	
-	print("[!] Total number of mails:", ct)
-	print("[!] Total number of leaked mails:", len(result))
-	time.sleep(2)
-	print("\n[!] DONE!")
-
-	# file output module will be added.
+		print("\n[!] DONE!")
+main()
