e1c1abca0aa0a367e50aa58d26e0758246edeb56
0xpr0N3rd <celik.anil@student.atilim.edu.tr>
2021-06-16 14:30:00 +0300

    Update pwnCheck.py

--- pwnCheck.py
+++ pwnCheck.py
@@ -6,6 +6,19 @@ def convertResponse(response): # convert tuple objects into string for sanitized
 	return functools.reduce(operator.add, (response)) 
 
 def main():
+	
+	print("""
+		              _____ _               _    
+		             / ____| |             | |   
+	 _ ____      ___ __ | |    | |__   ___  ___| | __
+	| '_ \ \ /\ / / '_ \| |    | '_ \ / _ \/ __| |/ /
+	| |_) \ V  V /| | | | |____| | | |  __/ (__|   < 
+	| .__/ \_/\_/ |_| |_|\_____|_| |_|\___|\___|_|\_\ 
+	| |                                              
+	|_|
+
+	""")
+
 	headers = {
 		'User-Agent': 'agent_smith',
 		'Accept': 'accept_em_all',
@@ -19,14 +32,19 @@ def main():
 	with open(sys.argv[1], 'r') as inputFile:
 		outputFile = open(sys.argv[2], 'w')
 		time.sleep(1)
+		
+		lineCount = len(open(sys.argv[1]).readlines( ))
+		
 		print("\n[!] File is being read...")
-		time.sleep(1)
+		time.sleep(3)
+		print("[!] Calculating total number of email addresses...")
+		print("[!] Total number of email addresses:", lineCount)
 		print("[!] Performing requests...\n")
 		print(lineBreaker)
 		
-		ct = 0
-		
-		for line in inputFile:
+		for ct, line in enumerate(inputFile):
+			ct += 1
+			
 			URL = 'https://haveibeenpwned.com/unifiedsearch/' # request URL
 			URL += line # append mail to the URL
 			r = requests.get(URL.strip(), headers=headers) # request object
@@ -37,13 +55,13 @@ def main():
 			breach_ct = 0 # hold breach counter
 			
 			if r.status_code == 200: # if status code is 200, it means mail is leaked 
-				succMsg = "[+] Found: ", line
-				print(convertResponse(succMsg))
+				succMsg = "[+] {0}/{1} Found: {2}" 
+				print(convertResponse(succMsg.format(str(ct), str(lineCount), str(line))))
 				result.append(line)
 				
 				with contextlib.redirect_stdout(outputFile): # redirect result to the output file
 					print(lineBreaker)
-					print(convertResponse(succMsg))
+					print(convertResponse(succMsg.format(str(ct), str(lineCount), str(line))))
 	
 				json_object = json.loads(r.text)
 				
@@ -51,7 +69,7 @@ def main():
 					breach_ct += 1
 				
 				for j in range(breach_ct): # iterate through breach to obtain the data 
-					leakMsg = "Leak ", str(i+1), ": ", json_object['Breaches'][i]['Name'], " - Breach Date: ", json_object['Breaches'][i]['BreachDate'] # integer to string parsing for concatenation error prevention
+					leakMsg = "Leak " + str(i+1) + ": " + json_object['Breaches'][i]['Name'] + " - Breach Date: " + json_object['Breaches'][i]['BreachDate'] # integer to string parsing for concatenation error prevention
 					print(convertResponse(leakMsg))
 					
 					with contextlib.redirect_stdout(outputFile): # redirect result to the output file
@@ -70,16 +88,16 @@ def main():
 				print(limitMsg)
 		
 			else:
-				notFound = "[-] Not Found: ", line
-				print(convertResponse(notFound))
+				notFound = "[-] {0}/{1} Not Found: {2}"
+				print(convertResponse(notFound.format(str(ct), str(lineCount), str(line))))
 				print(lineBreaker)
-			
-			ct+=1
+				
+			ct += 1
 		
-		firstResult = "[!] Total number of mails: ", str(ct)
+		firstResult = "[!] Total number of email addresses: ", str(lineCount)
 		print(convertResponse(firstResult))
 		
-		secondResult = "[!] Total number of leaked mails: ", str(len(result))
+		secondResult = "[!] Total number of leaked email addresses: ", str(len(result))
 		print(convertResponse(secondResult))
 		
 		with contextlib.redirect_stdout(outputFile): # redirect result to the output file
@@ -88,8 +106,14 @@ def main():
 			print(convertResponse(secondResult))
 		
 		print("\nPwned mails & breach information are written into:", sys.argv[2])
-		
-		time.sleep(2)
-	
 		print("\n[!] DONE!")
-main()
+
+if len(sys.argv) < 2:
+	print("\nType -h or --help for usage menu.\n")
+elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+	print("\nUsage:")
+	print("	\n# python3 pwnCheck.py <MAIL_LIST> <OUTPUT_FILE_NAME>\n")
+elif len(sys.argv) > 3 and sys.argv[1] != "-h" and sys.argv[1] != "--help":
+	print("\nToo many parameters amigo, try again.\n")
+else:
+	main()
